973c62b4f6aa64e68842a5908ad71a13e7d4466a
ChuckNezi <jnezia1@gmail.com>
2019-10-04 23:44:01 -0400

    ternary operators & switch statements

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -30,15 +30,61 @@
 //     console.log('John\'s BMI is higher than Marks\'s.');
 // }
 
+// var firstName = 'John';
+// var age = 16;
+
+// if(age < 13) {
+//    console.log(firstName + ' is a boy')
+// } else if(age >= 13 &&  age <= 19){
+//    console.log(firstName + ' is a teenager')
+// } else if(age >= 20 && age < 30){ 
+//    console.log(firstName + ' is a young man')
+// } else {
+//    console.log(firstName + ' is a man')
+// }
+
+
+/* Ternary Operators */
+
 var firstName = 'John';
 var age = 16;
 
-if(age < 13) {
-   console.log(firstName + ' is a boy')
-} else if(age >= 13 &&  age <= 19){
-   console.log(firstName + ' is a teenager')
-} else if(age >= 20 && age < 30){ 
-   console.log(firstName + ' is a young man')
-} else {
-   console.log(firstName + ' is a man')
+age >= 18 ? console.log(firstName + ' drinks beer') 
+: console.log(firstName + ' drinks juice')
+
+var drink = age >= 18 ? 'beer' : 'juice';
+console.log(drink);
+
+/* Switch Statement  */
+var job = 'MC';
+switch (job){
+   case 'teacher':
+   case 'instructor':
+      console.log(firstName + ' teaches kids to code');
+      break;
+   case 'driver':
+      console.log(firstName + ' drives an uber in Lisbon');
+      break;
+   case 'designer':
+      console.log(firstName + ' designs beautiful websites');
+      break;
+   default:
+      console.log(firstName + ' does something else.')
+}
+
+//New Age with Switch Statements
+
+age = 26;
+switch (true) {
+    case age < 13:
+        console.log(firstName + ' is a boy.');
+        break;
+    case age >= 13 && age < 20:
+        console.log(firstName + ' is a teenager.');
+        break;
+    case age >= 20 && age < 30:
+        console.log(firstName + ' is a young man.');
+        break;
+    default:
+        console.log(firstName + ' is a man.');
 }
